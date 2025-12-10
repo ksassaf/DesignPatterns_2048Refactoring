ccc3f063141f561379c30d706315c1936c3dc59a
ksassaf <kaschneider11@gmail.com>
2025-12-10 13:27:06 -0700

    Add files via upload

---
+++ View.java
@@ -0,0 +1,104 @@
+import javax.swing.JPanel;
+import java.awt.*;
+import java.util.List;
+import java.util.ArrayList;
+
+//refactored from  Konstantin Bulenkov
+
+	/**
+     * Observer interface and update method
+    View does nothing more than drawing the tiles of the model
+	and printing the result (win or lose).
+	no control logic, no event interpretation.
+	*/
+public class View extends JPanel implements Observer{
+    private static final Color BG_COLOR = new Color(0xbbada0);
+    private static final String FONT_NAME = "Arial";
+    private static final int TILE_SIZE = 64;
+    private static final int TILES_MARGIN = 16;
+  
+    private List<Tile> myTiles=new ArrayList<Tile>();  
+    boolean myWin = false;
+    boolean myLose = false;
+    int myScore = 0;
+
+	
+  /**
+     View needs to be consistent with the Model.
+     When Model changes --values of Tiles, Score, win and lose -
+	 this View has to change as well. 
+	 the 4 private fields need to be consistent with that of Model
+	 whenever any of them change.
+	 DO NOT add new fields to this class.
+	 Use a design pattern and add methods based on this pattern.
+	 */
+  
+	public void initialize(Controller controller){
+		setPreferredSize(new Dimension(340, 400));
+		setFocusable(true);
+		controller.resetGame();  
+	}
+
+    //no need to change these code.
+    public void paint(Graphics g) {
+        super.paint(g);
+        g.setColor(BG_COLOR);
+        g.fillRect(0, 0, this.getSize().width, this.getSize().height);
+        for (int y = 0; y < 4; y++) {
+        for (int x = 0; x < 4; x++) {
+            drawTile(g, myTiles.get(x + y * 4), x, y);
+        }
+        }
+    }
+
+    private void drawTile(Graphics g2, Tile tile, int x, int y) {
+        Graphics2D g = ((Graphics2D) g2);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_NORMALIZE);
+        int value = tile.value;
+        int xOffset = offsetCoors(x);
+        int yOffset = offsetCoors(y);
+        g.setColor(tile.getBackground());
+        g.fillRoundRect(xOffset, yOffset, TILE_SIZE, TILE_SIZE, 14, 14);
+        g.setColor(tile.getForeground());
+        final int size = value < 100 ? 36 : value < 1000 ? 32 : 24;
+        final Font font = new Font(FONT_NAME, Font.BOLD, size);
+        g.setFont(font);
+
+        String s = String.valueOf(value);
+        final FontMetrics fm = getFontMetrics(font);
+
+        final int w = fm.stringWidth(s);
+        final int h = -(int) fm.getLineMetrics(s, g).getBaselineOffsets()[2];
+
+        if (value != 0)
+        g.drawString(s, xOffset + (TILE_SIZE - w) / 2, yOffset + TILE_SIZE - (TILE_SIZE - h) / 2 - 2);
+
+        if (myWin || myLose) {
+        g.setColor(new Color(255, 255, 255, 30));
+        g.fillRect(0, 0, getWidth(), getHeight());
+        g.setColor(new Color(78, 139, 202));
+        g.setFont(new Font(FONT_NAME, Font.BOLD, 48));
+        if (myWin) {
+            g.drawString("You won!", 68, 150);
+        }
+        if (myLose) {
+            g.drawString("Game over!", 50, 130);
+            g.drawString("You lose!", 64, 200);
+        }
+        if (myWin || myLose) {
+            g.setFont(new Font(FONT_NAME, Font.PLAIN, 16));
+            g.setColor(new Color(128, 128, 128, 128));
+            g.drawString("Press ESC to play again", 80, getHeight() - 40);
+        }
+        }
+        g.setFont(new Font(FONT_NAME, Font.PLAIN, 18));
+        g.drawString("Score: " + myScore, 200, 365);
+
+    }
+
+    private static int offsetCoors(int arg) {
+        return arg * (TILES_MARGIN + TILE_SIZE) + TILES_MARGIN;
+    }
+
+  }
